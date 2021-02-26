98c608539c1d41a0e855a8ff2b1f7f7696ba34d4
jungai <juworaphol125@gmail.com>
2021-02-26 17:30:02 +0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { defineComponent } from 'vue'
+import './global.css'
 
 export default defineComponent({
   setup () {
     return () =>
       <div>
-        <p>Hello Vue 3 with tsx <span>👋</span></p>
+        <h1 class="text-lg underline text-red-400">Hello Vue 3 with tsx <span>👋</span></h1>
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link>
         <router-view/>
